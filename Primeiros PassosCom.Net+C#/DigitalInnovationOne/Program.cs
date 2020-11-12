b32b25d43cd1d13d9e0ecfe8a52fe0eb008ec98c
SuayMack <pry_chu@homail.com>
2020-11-12 16:54:32 -0300

    build project

--- Primeiros PassosCom.Net+C#/DigitalInnovationOne/Program.cs
+++ Primeiros PassosCom.Net+C#/DigitalInnovationOne/Program.cs
@@ -6,7 +6,12 @@ namespace DigitalInnovationOne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numeroDeVezes = 5;
+
+            for(int i = 0; i < numeroDeVezes; i++) {
+                Console.WriteLine($"Bem-vindo ao curso de .NET {i}");
+            }
+
         }
     }
 }
