bac208b63b549de0000dc29c93dfd7a439d3a2a2
SuayMack <pry_chu@homail.com>
2020-11-16 13:41:09 -0300

    update

---
+++ Primeiros PassosCom.Net+C#/EstruturaDoPrograma/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EstruturaDoPrograma.Exemplos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Pilha();
+            s.Empilha(1);
+            s.Empilha(10);
+            s.Empilha(100);
+
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+        }
+    }
+}
